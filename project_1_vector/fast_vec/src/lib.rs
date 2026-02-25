31bea9a72a9774f2db6cc2902f7c095c6d8850cb
Dhiraj <dhiraj1222@hotmail.com>
2026-02-25 14:10:03 -0500

    Implement remove function

--- project_1_vector/fast_vec/src/lib.rs
+++ project_1_vector/fast_vec/src/lib.rs
@@ -80,7 +80,19 @@ impl<T> FastVec<T> {
 
     // Student 1 should implement this.
     pub fn remove(&mut self, i: usize) {
-        todo!("implement remove");
+        if i >= self.len {
+            panic!("FastVec: remove out of bounds");
+        }
+        unsafe {
+            self.ptr_to_data.add(i).read();
+            for j in i+1..self.len {
+                let element = self.ptr_to_data.add(j).read();
+                let ptr: *mut T = self.ptr_to_data.add(j-1);
+                ptr.write(element);
+            }
+        }
+        self.len -= 1;
+
     }
 
     // This appears correct but with further testing, you will notice it has a bug!
