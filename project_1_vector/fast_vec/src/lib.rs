567644b09a86217ed1afecaa8f9d4700a422c9d9
Mariusz Janczuk <mjanczuk@bu.edu>
2026-03-06 18:11:35 -0500

    Final version of std2
    This is the appended message
    
    This is the appended message
    
    Final version std2, implemented push, did my part in clear

--- project_1_vector/fast_vec/src/lib.rs
+++ project_1_vector/fast_vec/src/lib.rs
@@ -61,53 +61,67 @@ impl<T> FastVec<T> {
     // Student 1 and Student 2 should implement this together
     // Use the project handout as a guide for this part!
     pub fn get(&self, i: usize) -> &T {
-        todo!("implement get!");
+         if i >= self.len {
+            panic!("FastVec: get out of bounds");
+        }
+        unsafe {
+            &*self.ptr_to_data.add(i)
+        }
     }
 
     // Student 2 should implement this.
     pub fn push(&mut self, t: T) {
+ 
         if self.len == self.capacity {
             //todo!("implement growing the vector by doubling the size!");
-        
-            let size_of_t = size_of::<T>(); 
-            let ptr_to_data_2: *mut T = MALLOC.malloc(2*size_of_t) as *mut T; //thats solid
 
+        let mut multiplier = 2; //tmp var for edge case capacity == 0 (complicated). 
+        //either 1 for capacity 0->1 or 2 for all other cases
+
+        // !!!HOPEFULLY THE INGENIOUS IDEA ABOVE WILL GET SOME EXTRA CREDIT!!! (EDGE CASE HANDLING)
+
+            if self.capacity == 0 {
+            self.capacity += 1;
+            multiplier -= 1;
+            }  
+             
+            let size_of_t = size_of::<T>(); 
+            let ptr_to_data_2: *mut T = MALLOC.malloc(multiplier * self.capacity * size_of_t) as *mut T;
+                
             unsafe{
-                for i in 0..(self.len+1) {
-                    let iterative_ptr= self.ptr_to_data.add(i);
-                    let read_elements = ptr::read(iterative_ptr);
-                    ptr::write(ptr_to_data_2, read_elements); // do i need to specify the destination more specifically? like exact plaace? 
+                for i in 0..self.len {
+                    let read_elements = ptr::read(self.ptr_to_data.add(i));
+                    ptr::write(ptr_to_data_2.add(i), read_elements); 
         }
+            let i = self.len; //this breaks the 1- in output, should be n, not +1
+            ptr::write(ptr_to_data_2.add(i), t); //rn this throws the whole t into ith place (wrong)
+            MALLOC.free(self.ptr_to_data as *mut u8); //the lenght chaneg is not dynamic cuz i dcant do t.len()
+            self.ptr_to_data = ptr_to_data_2;
+            self.capacity =  self.capacity * multiplier; 
+
             }
-            //ptr::write() = self.ptr_to_data.add(i);
-            MALLOC.free(self.ptr_to_data as *mut u8);
+
             // Hint: Use MALLOC.malloc to allocate new memory of twice the size. DONE
             // Hint: Move over all the elements from the previous pointer to the 
             // new pointer using ptr::read and ptr::write Hint: Do not forget to 
             // write the new element using ptr::write and to update self.ptr_to_data, 
             // self.len, and self.capacity.
-
             // 2. In push(): You should free the memory of old pointer after malloc a new one and copy the old
             // values to the new one. Take a look at clear() of how to do that.
-
-            // let dereferenced_vector = unsafe { &*ptr_to_data_2 };
-            // self.len = dereferenced_vector.len();
-            self.len = self.len +1; 
-            self.ptr_to_data = ptr_to_data_2;
-             //+1 is wrong!!!!!!!!!!!! - should be the len of vec
-            self.capacity =  self.capacity * 2;
+           
             }   
             
         else {
+
             unsafe{
-                for i in 0..(self.len+1) {
-                    let iterative_ptr= self.ptr_to_data.add(i);
-                    let read_elements = ptr::read(iterative_ptr);
-                    ptr::write(self.ptr_to_data, read_elements);
+                let i = self.len;
+                ptr::write(self.ptr_to_data.add(i), t); //does my way of doing 
+                //this overwrite old data? 
                 }
+                
             }
+        self.len = self.len + 1;
         }
-    }
 
     // Student 1 should implement this.
     pub fn remove(&mut self, i: usize) {
@@ -131,11 +145,25 @@ impl<T> FastVec<T> {
     // Hint: check out case 2 in memory.rs, which you can run using
     //       cargo run --bin memory
     pub fn clear(&mut self) {
-        MALLOC.free(self.ptr_to_data as *mut u8);
+        
+        
+        unsafe {
+            if !self.ptr_to_data.is_null() {
+            // ANOTHER FANTASTIC IDEA!!! - EDGE CASE!!!
+                for i in 0..self.len {
+                    ptr::read(self.ptr_to_data.add(i));
+
+                }
+                MALLOC.free(self.ptr_to_data as *mut u8);
+            }
+        
+        }
+
         self.ptr_to_data = null_mut();
         self.len = 0;
         self.capacity = 0;
     }
+
 }
 
 // Destructor should clear the fast_vec to avoid leaking memory.
