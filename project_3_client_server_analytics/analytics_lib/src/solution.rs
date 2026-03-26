278b20cd1f6aa77283937c0a70f40712f31c5f52
Dhiraj <dhiraj1222@hotmail.com>
2026-03-26 19:38:23 -0400

    Implement satisfies_condition (helper function) and filter_database

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,8 +2,41 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+//helper function that checks if a row satifies a condition
+//takes a row, dataset, and condition as arguments
+//returns true if the row satifies the condtion, returns false if the row does not satisfy the condition
+pub fn satisfies_condition(row: &Row, dataset: &Dataset, condition: &Condition) -> bool {
+    let mut bool = false; 
+    match condition {
+        Condition::Equal(column_name, value) => {
+            let index = dataset.column_index(column_name);
+            let item = row.get_value(index);
+            if item == value {
+                bool = true;
+            }
+        },
+        Condition::Not(condition) => {
+            bool = !satisfies_condition(row, dataset, condition);
+        },
+        Condition::And(condition_1, condition_2) => {
+            bool = satisfies_condition(row, dataset, condition_1) && satisfies_condition(row, dataset, condition_2);
+        }, 
+        Condition::Or(condition_1, condition_2) => {
+            bool = satisfies_condition(row, dataset, condition_1) || satisfies_condition(row, dataset, condition_2);
+        }
+    }
+
+    return bool
+}
+
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let mut out = Dataset::new(dataset.columns().clone());
+    for row in dataset.iter() {
+           if satisfies_condition(row, dataset, filter) == true {
+            out.add_row(row.clone());
+           }
+    }
+ return out;
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
