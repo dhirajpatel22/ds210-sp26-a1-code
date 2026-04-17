5e95c785c58a56c2eb71f87e4a540b5e1d92713f
Dhiraj <dhiraj1222@hotmail.com>
2026-04-17 16:40:19 -0400

    Implement basic heuristic function (board.score)

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -1,6 +1,6 @@
 use tic_tac_toe_stencil::agents::Agent;
 use tic_tac_toe_stencil::board::Board;
-use tic_tac_toe_stencil::player::Player;
+use tic_tac_toe_stencil::player::Player::{self, O, X};
 
 // Your solution solution.
 pub struct SolutionAgent {}
@@ -11,6 +11,42 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-        unimplemented!("Not yet implemented")
+        // If you want to make a recursive call to this solution, use
+        // `SolutionAgent::solve(...)`
+        if board.game_over() {
+            return (board.score(), 0, 0);
+        }
+
+        let moves = board.moves();
+        let mut best_move: (usize, usize) = (0, 0);
+        let mut best_score: i32 = match player {
+            X => i32::MIN,
+            O => i32::MAX,
+        };
+
+        for possible_move in moves {
+            board.apply_move(possible_move, player);
+
+            let (future_score, _, _) =
+                SolutionAgent::solve(board, player.flip(), _time_limit);
+
+            board.undo_move(possible_move, player);
+
+            let better = match player {
+                X => future_score > best_score,
+                O => future_score < best_score,
+            };
+
+            if better {
+                best_score = future_score;
+                best_move = possible_move;
+            }
+        }
+
+        return (best_score, best_move.0, best_move.1);
     }
 }
+
+fn heuristic(board: &Board) -> i32 {
+        return board.score()
+    }
